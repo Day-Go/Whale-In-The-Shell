e4082371989144521eb9bf599e6b79abeab4bce3
Day-Go <lucarusso.521@gmail.com>
2023-12-02 18:56:36 +1100

    basic data getting eg

---
+++ WitsFrontend/Models/Prompts.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Postgrest.Attributes;
+using Postgrest.Models;
+
+namespace WitsFrontend.Models;
+
+[Table("prompts")]
+public class Prompt : BaseModel
+{
+    [PrimaryKey("id", false)]
+    public int Id { get; set; }
+
+    [Column("name")]
+    public string? Name { get; set; }
+
+    [Column("content")]
+    public string? Content { get; set; }
+
+}
